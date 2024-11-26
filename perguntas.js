8b6c9f9f6cfab908ca7417f41f1b0aef70be0961
Geovanna Rodrigues <0000109380905Xsp@al.educacao.sp.gov.br>
2024-11-26 07:23:02 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filmes de Terror',
+    'Qual foi o primeiro filme de terror em cores? ',
+    'A maldição de frankenstein'
+)
+
+criaCartao(
+    'Filmes de Comédia',
+    'Para que ano Marty e Doc viajam em "De Volta para o Futuro Parte II"?',
+    '2015'
+)
+
+criaCartao(
+    'Filmes de Romance',
+    'Em 13 Going On 30, para qual revista Jenna trabalha?',
+    'Equilíbrio'
+)
+
+criaCartao(
+    'Filmes de Terror',
+    'Que cor está presente em quase todas as cenas de O Iluminado?',
+    'Vermelho'
+)
